55540baaf88cc143b4e2a7e3941e51b1f06cb1f1
Dave Vronay <davevr@eweware.com>
2016-09-25 22:20:21 -0700

    New views

--- PhabrikAndroid/Resources/Resource.Designer.cs
+++ PhabrikAndroid/Resources/Resource.Designer.cs
@@ -2233,97 +2233,103 @@ namespace Phabrik.AndroidApp
 			public const int ArmyDetailLayout = 2130903066;
 			
 			// aapt resource value: 0x7f03001b
-			public const int DrawerListItem = 2130903067;
+			public const int ArmyListLayout = 2130903067;
 			
 			// aapt resource value: 0x7f03001c
-			public const int FleetDetailLayout = 2130903068;
+			public const int DrawerListItem = 2130903068;
 			
 			// aapt resource value: 0x7f03001d
-			public const int FleetListLayout = 2130903069;
+			public const int FleetDetailLayout = 2130903069;
 			
 			// aapt resource value: 0x7f03001e
-			public const int GalaxyPopLayout = 2130903070;
+			public const int FleetListLayout = 2130903070;
 			
 			// aapt resource value: 0x7f03001f
-			public const int GameLayout = 2130903071;
+			public const int GalaxyPopLayout = 2130903071;
 			
 			// aapt resource value: 0x7f030020
-			public const int GridSectorLayout = 2130903072;
+			public const int GameLayout = 2130903072;
 			
 			// aapt resource value: 0x7f030021
-			public const int LoginLayout = 2130903073;
+			public const int GridSectorLayout = 2130903073;
 			
 			// aapt resource value: 0x7f030022
-			public const int Main = 2130903074;
+			public const int LoginLayout = 2130903074;
 			
 			// aapt resource value: 0x7f030023
-			public const int notification_media_action = 2130903075;
+			public const int Main = 2130903075;
 			
 			// aapt resource value: 0x7f030024
-			public const int notification_media_cancel_action = 2130903076;
+			public const int notification_media_action = 2130903076;
 			
 			// aapt resource value: 0x7f030025
-			public const int notification_template_big_media = 2130903077;
+			public const int notification_media_cancel_action = 2130903077;
 			
 			// aapt resource value: 0x7f030026
-			public const int notification_template_big_media_narrow = 2130903078;
+			public const int notification_template_big_media = 2130903078;
 			
 			// aapt resource value: 0x7f030027
-			public const int notification_template_lines = 2130903079;
+			public const int notification_template_big_media_narrow = 2130903079;
 			
 			// aapt resource value: 0x7f030028
-			public const int notification_template_media = 2130903080;
+			public const int notification_template_lines = 2130903080;
 			
 			// aapt resource value: 0x7f030029
-			public const int notification_template_part_chronometer = 2130903081;
+			public const int notification_template_media = 2130903081;
 			
 			// aapt resource value: 0x7f03002a
-			public const int notification_template_part_time = 2130903082;
+			public const int notification_template_part_chronometer = 2130903082;
 			
 			// aapt resource value: 0x7f03002b
-			public const int NotifyTabView = 2130903083;
+			public const int notification_template_part_time = 2130903083;
 			
 			// aapt resource value: 0x7f03002c
-			public const int PlanetListItemLayout = 2130903084;
+			public const int NotifyTabView = 2130903084;
 			
 			// aapt resource value: 0x7f03002d
-			public const int PlanetPopLayout = 2130903085;
+			public const int PlanetListItemLayout = 2130903085;
 			
 			// aapt resource value: 0x7f03002e
-			public const int PointOfPresenceLayout = 2130903086;
+			public const int PlanetPopLayout = 2130903086;
 			
 			// aapt resource value: 0x7f03002f
-			public const int ProfileLayout = 2130903087;
+			public const int PointOfPresenceLayout = 2130903087;
 			
 			// aapt resource value: 0x7f030030
-			public const int psts_tab = 2130903088;
+			public const int ProfileLayout = 2130903088;
 			
 			// aapt resource value: 0x7f030031
-			public const int SectorPopLayout = 2130903089;
+			public const int psts_tab = 2130903089;
 			
 			// aapt resource value: 0x7f030032
-			public const int select_dialog_item_material = 2130903090;
+			public const int ResourceListLayout = 2130903090;
 			
 			// aapt resource value: 0x7f030033
-			public const int select_dialog_multichoice_material = 2130903091;
+			public const int SectorPopLayout = 2130903091;
 			
 			// aapt resource value: 0x7f030034
-			public const int select_dialog_singlechoice_material = 2130903092;
+			public const int select_dialog_item_material = 2130903092;
 			
 			// aapt resource value: 0x7f030035
-			public const int ShipDetailLayout = 2130903093;
+			public const int select_dialog_multichoice_material = 2130903093;
 			
 			// aapt resource value: 0x7f030036
-			public const int SolSysPopLayout = 2130903094;
+			public const int select_dialog_singlechoice_material = 2130903094;
 			
 			// aapt resource value: 0x7f030037
-			public const int StructurePopLayout = 2130903095;
+			public const int ShipDetailLayout = 2130903095;
 			
 			// aapt resource value: 0x7f030038
-			public const int support_simple_spinner_dropdown_item = 2130903096;
+			public const int SolSysPopLayout = 2130903096;
 			
 			// aapt resource value: 0x7f030039
-			public const int TroopDetailLayout = 2130903097;
+			public const int StructurePopLayout = 2130903097;
+			
+			// aapt resource value: 0x7f03003a
+			public const int support_simple_spinner_dropdown_item = 2130903098;
+			
+			// aapt resource value: 0x7f03003b
+			public const int TroopDetailLayout = 2130903099;
 			
 			static Layout()
 			{
